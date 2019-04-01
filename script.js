b4bf7fa16715e279c5a3dfa00ed8224b1e9bb6a0
Darcy LF <ehnevault@gmail.com>
2019-04-02 08:11:56 +1100

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function millisToMinutes(millis) {
 Zepto(function($){
     
     // the url of the ptv api
-    var url = "http://timetableapi.ptv.vic.gov.au/v3/departures/route_type/0/stop/1152?platform_numbers=2&include_cancelled=true&expand=disruption&devid=3001117&signature=CEBF7AED8AE6BEBCC8C5E0F5B28A38AB8A919EC4";
+    var url = "https://timetableapi.ptv.vic.gov.au/v3/departures/route_type/0/stop/1152?platform_numbers=2&include_cancelled=true&expand=disruption&devid=3001117&signature=CEBF7AED8AE6BEBCC8C5E0F5B28A38AB8A919EC4";
     
     $.getJSON(url, function(data){
         
@@ -52,7 +52,7 @@ Zepto(function($){
             //edit #dataContainer html
             $('#dataContainer').html(time + " " + pluralize('minute', time));  
         }
-        $.getJSON("http://timetableapi.ptv.vic.gov.au/v3/pattern/run/952519/route_type/0?expand=all&stop_id=1152&devid=3001117&signature=E2B25BE9E0C13B343BA584029755B8B2097C1F13", function(data){
+        $.getJSON("https://timetableapi.ptv.vic.gov.au/v3/pattern/run/952519/route_type/0?expand=all&stop_id=1152&devid=3001117&signature=E2B25BE9E0C13B343BA584029755B8B2097C1F13", function(data){
             alert("aaaaahhh "+new Date('2019-04-01T09:33:00Z'))
         });
         
